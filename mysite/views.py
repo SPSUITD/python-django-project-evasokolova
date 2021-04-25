2b094175ccec0bfba155f79229ddb7ad5735f994
evasokolova <>
2021-04-25 11:57:44 +0000

    Добавила файл views, а в файл urls импортировала views

---
+++ mysite/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+def test(request):
+    context = {'name':request.GET['name']}
+    return render(request, 'mysite/test.html',context)
+# request.GET['name']
+def form(request):
+    return render(request, 'mysite/form.html')
